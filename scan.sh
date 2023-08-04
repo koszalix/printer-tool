594ac18f28808c8a031ecc54e6b49fd556fbc174
pawel koszalka <koszalix@gmail.com>
2023-08-04 23:25:30 +0200

    Add filename creator

--- scan.sh
+++ scan.sh
@@ -4,6 +4,31 @@
 # Normal quality
 # PDF
 
-function A4_fast_pdf() {
-  hp-scan --res=300 --size=a4 --dest=pdf
+OUT="${HOME}"
+
+function _filename () {
+  base_name=$(date +'%d-%m-%Y_%H-%M-%S')
+  counter=""
+
+  while true; do
+    if [[ $counter == "" ]]
+    then
+      filename="scan_${base_name}$1"
+    else
+      filename="scan_${base_name}_${counter}$1"
+    fi
+    if [[ ! -e "$filename" ]]; then
+      return $filename
+    fi
+    ((counter++))
+  done
 }
+
+
+
+function A4_fast_pdf {
+  _filename .pdf
+  hp-scan --res=300 --size=a4 --dest=pdf --file=$HOME/Scans/$filename
+}
+
+A4_fast_pdf
